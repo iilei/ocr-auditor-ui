2cca5c01c2eb4a8efa07150aabf56c044b7abffe
iilei • Jochen Preusche <iilei@users.noreply.github.com>
2019-10-12 12:23:28 +0200

    boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace;
+  }
+`;
+
+const App: React.FC = () => {
+  return (
+    <React.Fragment>
+      <GlobalStyle />
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+            Learn React
+          </a>
+        </header>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default App;
