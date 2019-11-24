4e27831c8c0472c1b794f02943dfb1540264ca0b
iilei • Jochen Preusche <iilei@users.noreply.github.com>
2019-11-24 12:02:31 +0100

    Style

--- src/plugins/shapes/_constants.ts
+++ src/plugins/shapes/_constants.ts
@@ -5,5 +5,5 @@ export const tokens = ['careas', 'pars', 'lines', 'words'];
 export const defaultOptions = { snappy: true, padding: { left: 12, lineEnd: 12 } };
 export const renderOptions = {
   outer: { fill: 'turquoise', opacity: 0.1, globalCompositeOperation: 'multiply' },
-  inner: { stroke: 'doggerblue', opacity: 0.4, globalCompositeOperation: 'multiply', strokeWidth: 1 },
+  inner: { stroke: 'turquoise', opacity: 0.8, globalCompositeOperation: 'multiply', strokeWidth: 1 },
 };
